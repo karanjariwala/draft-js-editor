09246b1731b6d575afcd5467da511c3e1bc265fb
karanjariwala <karanjariwala@gmail.com>
2018-12-10 16:31:05 +0530

    adding cmd+h line

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {  EditorState, RichUtils} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createHighlightPlugin from './highlightPlugin';
+
+const highlightPlugin = createHighlightPlugin({
+  background: 'purple'
+});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  onChange = (editorState) => {
+    this.setState({editorState})
+  }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+
+    if(newState){
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState));
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <button onClick={this.onUnderlineClick}>Underline</button>
+        <button onClick={this.onToggleCode}>Code Block</button>
+        <div style={{display:'inline'}} > cmd +h to highlight</div>
+        <div className='Editor'>
+        <Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+        handleKeyCommand={this.handleKeyCommand}
+        plugins={[highlightPlugin]}
+      />
+        </div>
+        
       </div>
     );
   }
 }
 
 export default App;
+
