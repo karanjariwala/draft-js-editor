f10012d3d5ee72134d958e178cdb979529fdb9f3
karanjariwala <karanjariwala@gmail.com>
2018-12-10 12:01:33 +0530

    Updates

---
+++ precache-manifest.e5fefaa06e660f77cc03785086bffc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea01db28181a54c384d8",
+    "url": "/draft-js-editor/static/js/runtime~main.ea01db28.js"
+  },
+  {
+    "revision": "91d6146e45a033cf3d6d",
+    "url": "/draft-js-editor/static/js/main.91d6146e.chunk.js"
+  },
+  {
+    "revision": "ed3cd981203417442e7a",
+    "url": "/draft-js-editor/static/js/1.ed3cd981.chunk.js"
+  },
+  {
+    "revision": "91d6146e45a033cf3d6d",
+    "url": "/draft-js-editor/static/css/main.63c2fce9.chunk.css"
+  },
+  {
+    "revision": "b52e07dc614b2733102aa41de95a1bab",
+    "url": "/draft-js-editor/index.html"
+  }
+];
